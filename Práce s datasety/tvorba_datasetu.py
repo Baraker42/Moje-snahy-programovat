56e36632b66316f1ea4923abab601620003f4d2b
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2020-09-18 11:17:44 +0200

    Add files via upload

--- Práce s datasety/tvorba_datasetu.py
+++ Práce s datasety/tvorba_datasetu.py
@@ -5,40 +5,44 @@ import json
 oblast=["Praha","Brno","Ostrava","Olomouc","Zlin"]
 druh=["Tesarik krovovy","Tesarik pizmovy","Patericek zluty","Zlatohlavek zlaty","Rohac obecny"]
 pocet=0
-cil=int(input("Zadej počet náhodně vygenerovaných záznamů "))
-oblasti=[]
-vyskyty=[]
-druhy=[]
-while pocet < cil:
-    rand_oblast=random.choice(oblast)
-    oblasti.append(rand_oblast)
-    rand_druh=random.choice(druh)
-    druhy.append(rand_druh)
-    vyskyt=randrange(0,20)
-    vyskyty.append(vyskyt)
-    pocet=pocet+1
+error=1
+while error == 1:
+    try:
+        cil=int(input("Zadej počet náhodně vygenerovaných záznamů "))
+        oblasti=[]
+        vyskyty=[]
+        druhy=[]
+        while pocet < cil:
+            rand_oblast=random.choice(oblast)
+            oblasti.append(rand_oblast)
+            rand_druh=random.choice(druh)
+            druhy.append(rand_druh)
+            vyskyt=randrange(0,20)
+            vyskyty.append(vyskyt)
+            pocet=pocet+1
 
-celek=[]
-zaznam=0
-delka=len(oblasti)
-while zaznam < delka:
-    lokace={}
-    druh_n=druhy[zaznam]
-    oblast_n=oblasti[zaznam]
-    vyskyt_n=vyskyty[zaznam]
+        celek=[]
+        zaznam=0
+        delka=len(oblasti)
+        while zaznam < delka:
+            druh_n=druhy[zaznam]
+            oblast_n=oblasti[zaznam]
+            vyskyt_n=vyskyty[zaznam]
+            lokace={"Druh": druh_n,"Oblast": oblast_n,"Vyskyt":vyskyt_n,"Lokace":zaznam}
+            celek.append(lokace)   
+            zaznam=zaznam+1
+
+
+
+
+        with open ("././/zaznamy.json", "w") as file:
+            json.dump(celek, file)
+        error=0
+        print("Bylo vygenerováno",cil,"záznamů")
+    except ValueError:
+        print("Neplatná hodnota")
+        error=1
     
-    lokace["Druh"]=druh_n
-    lokace["Oblast"]=oblast_n
-    lokace["Vyskyt"]=vyskyt_n
-    lokace["Lokace"]=zaznam
-    celek.append(lokace)   
-    zaznam=zaznam+1
-
-
-
-
-with open ("././/zaznamy.json", "w") as file:
-    json.dump(celek, file)
 
     
 
