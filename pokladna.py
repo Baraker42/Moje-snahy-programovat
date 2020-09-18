e086531060c86975493edc136c34e45e8069bb67
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2020-09-18 09:33:17 +0200

    Add files via upload

--- pokladna.py
+++ pokladna.py
@@ -1,7 +1,7 @@
-print ("Pokladna 0.1 - Alfa verze")
+print ("Pokladna 0.3 - Alfa verze")
 pokladna = int(input("Kolik je na začátku dne v pokladně? "))
 pokladna1=pokladna
-cena=""
+
 celkova_cena=0
 pocet_zakazniku=0
 den="Další"
@@ -13,16 +13,19 @@ while den =="Další":
         except ValueError:
             break
     print("Nákup stojí", celkova_cena)
-    platba = int(input("Zákazník platí "))
+    try:
+        
+        platba = int(input("Zákazník platí "))
+    except ValueError:
+        print("Byla zadána neplatná hodnota")
+        platba = int(input("Zákazník platí "))
     vratit=platba-celkova_cena
     print("Vraťte zákazníkovi", vratit)
     input()
     pokladna = pokladna+(platba-vratit)
-    print(pokladna)
+    print("Aktuální stav pokladny:"pokladna)
     pocet_zakazniku=pocet_zakazniku + 1
     celkova_cena=0
-    cena=""
-    print(cena)
     den = input("Další/Konec ")
 print("Celkový počet zákazníků za dnešek je", pocet_zakazniku)
 print("V pokladně se momentálně nachází", pokladna)
